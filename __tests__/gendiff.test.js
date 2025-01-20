a251282a8014ac0be88522c4a586e48a5da09524
Makkinni <efstas187@gmail.com>
2025-01-20 14:52:19 +0300

    fix tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -16,22 +16,28 @@ test('Сравнение двух JSON-файлов stylish format', () => {
   const data1 = readFile('__fixtures__/file1.json');
   const data2 = readFile('__fixtures__/file2.json');
   const expected = readFixtureFile('result.txt');
-
-  expect(buildDifference(data1, data2, 'stylish')).toEqual(expected);
+  expect(normalizedStr(buildDifference(data1, data2, 'stylish'))).toEqual(normalizedStr(expected));
 });
-// test('Сравнение двух JSON-файлов Plain format', () => {
-//   const data1 = readFile('__fixtures__/file1.json');
-//   const data2 = readFile('__fixtures__/file2.json');
-//   const expected = readFixtureFile('resultPlain.txt');
-//   console.log(buildDifference(data1, data2, 'plain'));
-//
-//   console.log(expected);
-//   expect(normalizedStr(buildDifference(data1, data2, 'plain'))).toEqual(normalizedStr(expected));
-// });
-//
-// test('Сравнение двух YAML-файлов', () => {
-//   const data1 = readFixtureFile('file1.yml');
-//   const data2 = readFixtureFile('file2.yml');
-//   const expected = readFixtureFile('result.txt');
-//   expect(buildDifference(data1, data2)).toEqual(expected.trim());
-// });
+test('Сравнение двух JSON-файлов plain format', () => {
+  const data1 = readFile('__fixtures__/file1.json');
+  const data2 = readFile('__fixtures__/file2.json');
+  const expected = readFixtureFile('resultPlain.txt');
+  console.log(buildDifference(data1, data2, 'plain'));
+
+  console.log(expected);
+  expect(normalizedStr(buildDifference(data1, data2, 'plain'))).toEqual(normalizedStr(expected));
+});
+
+test('Сравнение двух YAML-файлов stylish format', () => {
+  const data1 = readFile('__fixtures__/file1.yml');
+  const data2 = readFile('__fixtures__/file2.yml');
+  const expected = readFixtureFile('result.txt');
+  expect(normalizedStr(buildDifference(data1, data2, 'stylish'))).toEqual(normalizedStr(expected));
+});
+
+test('Сравнение двух YAML-файлов plain format', () => {
+  const data1 = readFile('__fixtures__/file1.yml');
+  const data2 = readFile('__fixtures__/file2.yml');
+  const expected = readFixtureFile('resultPlain.txt');
+  expect(normalizedStr(buildDifference(data1, data2, 'plain'))).toEqual(normalizedStr(expected));
+});
