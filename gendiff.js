1df1e953d00dc9a92528919ec0d37682c208aa5b
Makkinni <efstas187@gmail.com>
2025-01-20 17:36:06 +0300

    fix missing paths error

--- gendiff.js
+++ gendiff.js
@@ -12,6 +12,10 @@ program
   .option('-f, --format [type]', 'output format', 'stylish')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2, options) => {
+    if (!filepath1 || !filepath2) {
+      console.error('Error: Missing required file paths.');
+      process.exit(1);
+    }
     const data1 = readFile(filepath1);
     const data2 = readFile(filepath2);
     const result = buildDifference(data1, data2, options.format);
